f1e3a0007a502633ba5ac9539e54965c34cd3302
Luis Lopez <ingluislopezh@outlook.com>
2026-04-10 08:10:30 -0500

    Primer commit proyecto limpio 0010

--- GenerateTest/tests/Mercadolibre_test/accessibility/Mercadolibre_test-accessibility.spec.ts
+++
@@ -1,9 +0,0 @@
-
-import { test, expect } from '@playwright/test';
-import AxeBuilder from '@axe-core/playwright';
-
-test('accessibility Mercadolibre_test', async ({ page }) => {
-  await page.goto('https://www.mercadolibre.com.co');
-  const results = await new AxeBuilder({ page }).analyze();
-  console.log("Violations:", results.violations.length);
-});
